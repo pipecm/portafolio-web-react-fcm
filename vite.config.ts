56270fdc4856319175e32bde36e4a3262bf6ceef
Felipe <pipecm@mbp-pipecm.local>
2025-04-10 21:51:10 -0400

    Editada configuracion de sitio

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: [ 'ec2-18-212-165-212.compute-1.amazonaws.com' ]
   },
   plugins: [react()],
   resolve: {
